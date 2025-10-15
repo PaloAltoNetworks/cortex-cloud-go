43be9aaff6e921e1dc48ea52be758f208011bbe6
Max Boynton <mboynton@paloaltonetworks.com>
2025-10-15 13:39:24 -0500

    chore: add commented-out logic for changing test server URL into a format that is similar to a real Cortex API URL

--- platform/helpers_test.go
+++ platform/helpers_test.go
@@ -1,20 +1,37 @@
 package platform
 
 import (
+	//"fmt"
 	"net/http"
 	"net/http/httptest"
+	//"net/url"
 	"testing"
 
 	"github.com/PaloAltoNetworks/cortex-cloud-go/log"
 	"github.com/stretchr/testify/assert"
 )
 
+//func testServerURLtoCortexAPI(serverURL string) (string, error) {
+//	parsedURL, err := url.Parse(serverURL)
+//	if err != nil {
+//		return "", err
+//	}
+//	
+//	return fmt.Sprintf("https://api-%s", parsedURL.Hostname()), nil
+//}
+
 func setupTest(t *testing.T, handler http.HandlerFunc) (*Client, *httptest.Server) {
 	t.Helper()
 
-	t.Logf("Creating test server for %s", t.Name())
-
 	server := httptest.NewServer(handler)
+	//cortexAPIURL, err := testServerURLtoCortexAPI(server.URL)
+	//if err != nil {
+	//	t.Fatalf("failed to convert test server URL: %s", err.Error())
+	//}
+	//server.URL = cortexAPIURL
+
+	t.Logf("[%s] Test server URL %s", t.Name(), server.URL)
+
 	client, err := NewClient(
 		WithCortexAPIURL(server.URL),
 		WithCortexAPIKey("test-key"),
