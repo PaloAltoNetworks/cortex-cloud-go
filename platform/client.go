ee17863368ddafba629b817217c41bde84a89e80
Max Boynton <mboynton@paloaltonetworks.com>
2025-11-06 11:33:51 -0600

    chore: spacing + removing note with link to non-public API portal

--- platform/client.go
+++ platform/client.go
@@ -31,6 +31,7 @@ const (
 	UserGroupEndpoint = "platform/iam/v1/user-group"
 	IamUsersEndpoint  = "platform/iam/v1/user"
 	ScopeEndpoint     = "platform/iam/v1/scope"
+
 	// Asset Group Endpoints
 	CreateAssetGroupEndpoint = "public_api/v1/asset-groups/create"
 	UpdateAssetGroupEndpoint = "public_api/v1/asset-groups/update/"
@@ -38,7 +39,6 @@ const (
 	ListAssetGroupsEndpoint  = "public_api/v1/asset-groups"
 
 	// Auth Settings Endpoints
-	// Updated on Oct 30 per https://cortex-panw.stoplight.io/docs/cortex-cloud/ endpoint change
 	ListIDPMetadataEndpoint    = "public_api/v1/authentication-settings/get/metadata"
 	ListAuthSettingsEndpoint   = "public_api/v1/authentication-settings/get/settings"
 	CreateAuthSettingsEndpoint = "public_api/v1/authentication-settings/create"
